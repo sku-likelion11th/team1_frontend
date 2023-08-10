644d9a34aab426aee50d38f73ec870fe0b704909
sieun <charm516@naver.com>
2023-08-10 22:33:18 +0900

    sharing 버튼 병합

---
+++ js/animation_sharebutton.js
@@ -0,0 +1,6 @@
+const shareBtn = document.querySelector(".share-btn");
+const shareOptions = document.querySelector(".share-options");
+
+shareBtn.addEventListener("click", function () {
+  shareOptions.classList.toggle("active");
+});
