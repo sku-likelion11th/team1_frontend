cd476cce2a09676952a794a4516830577541a392
sieun <charm516@naver.com>
2023-08-14 04:46:20 +0900

    FIX: writing page modal 오류 수정

--- js/modal.js
+++ js/modal.js
@@ -1,28 +1,42 @@
 const tapeImages = document.querySelectorAll(".modal-on");
-const tapeChange = document.querySelector(".tapeChange");
-
 const modalOverlay = document.getElementById("modal-overlay");
 const closeBtn = modal.querySelector(".close-area");
+const tapeChange = document.querySelector(".tapeChange");
+const changeBttn = document.querySelector(".change-bttn");
+const tapeBox = document.querySelector(".tape-box");
+const noneModalTapeImages = document.querySelector(".page>img");
 
-tapeImages.forEach((image) => {
-  image.addEventListener("click", () => {
+if (tapeImages) {
+  tapeImages.forEach((image) => {
+    image.addEventListener("click", () => {
+      modalOn();
+    });
+  });
+}
+
+if (tapeChange) {
+  tapeChange.addEventListener("click", () => {
     modalOn();
   });
-});
-
-tapeChange.addEventListener("click", () => {
-  modalOn();
-});
-
+}
 function modalOn() {
   modal.style.display = "flex";
   modalOverlay.style.display = "block";
+  tapeBox.style.display = "none";
+  noneModalTapeImages.style.display = "none";
 }
 
 function modalOff() {
   modalOverlay.style.display = "none";
+  modal.style.display = "none";
+  tapeBox.style.display = "block";
+  noneModalTapeImages.style.display = "block";
 }
-
+if (changeBttn) {
+  changeBttn.addEventListener("click", () => {
+    modalOff();
+  });
+}
 document.addEventListener("keydown", (e) => {
   if (e.key === "Escape") {
     modalOff();
@@ -30,5 +44,9 @@ document.addEventListener("keydown", (e) => {
 });
 
 closeArea.addEventListener("click", () => {
+  modalOff();
+});
+
+changeBttn.addEventListener("click", () => {
   modalOff();
 });
