bd6caf4f974d55e319d76579c8732156bc8feddf
sieun <charm516@naver.com>
2023-08-16 01:35:26 +0900

    FEAT : 음악선택 모달창 구현 완료(기능x)

--- js/writingModal.js
+++ js/writingModal.js
@@ -1,38 +1,43 @@
 const tapeChange = document.querySelector(".tapeChange");
 const musicChange = document.querySelector(".musicChange");
 
-const modalOverlay = document.querySelector(".modal-overlay");
+const tapeOverlay = document.getElementById("tape-overlay");
+const musicOverlay = document.getElementById("music-overlay");
+
 const modalTape = document.getElementById("modal-tape");
 const modalMusic = document.getElementById("modal-music");
 
-const closeBttn = document.querySelector(".closeBttn");
+const closeBttns = document.querySelectorAll(".closeBttn");
 
-const changeBttn = document.querySelector(".change-bttn");
 const tapeBox = document.querySelector(".tape-box");
 const noneModalTapeImages = document.querySelector(".page>img");
 
 function modalTapeOn() {
   modalTape.style.display = "flex";
-  modalOverlay.style.display = "block";
+  tapeOverlay.style.display = "block";
+  musicOverlay.style.display = "block";
   tapeBox.style.display = "none";
   noneModalTapeImages.style.display = "none";
 }
 function modalMusicOn() {
   modalMusic.style.display = "flex";
-  modalOverlay.style.display = "block";
+  tapeOverlay.style.display = "block";
+  musicOverlay.style.display = "block";
   tapeBox.style.display = "none";
   noneModalTapeImages.style.display = "none";
 }
 
 function modalTapeOff() {
   modalTape.style.display = "none";
-  modalOverlay.style.display = "none";
+  tapeOverlay.style.display = "none";
+  musicOverlay.style.display = "none";
   tapeBox.style.display = "block";
   noneModalTapeImages.style.display = "block";
 }
 function modalMusicOff() {
   modalMusic.style.display = "none";
-  modalOverlay.style.display = "none";
+  tapeOverlay.style.display = "none";
+  musicOverlay.style.display = "none";
   tapeBox.style.display = "block";
   noneModalTapeImages.style.display = "block";
 }
@@ -55,7 +60,9 @@ document.addEventListener("keydown", (e) => {
   }
 });
 
-closeBttn.addEventListener("click", () => {
-  modalTapeOff();
-  modalMusicOff();
+closeBttns.forEach((closeBttn) => {
+  closeBttn.addEventListener("click", () => {
+    modalTapeOff();
+    modalMusicOff();
+  });
 });
